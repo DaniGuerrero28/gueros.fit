13d9daa8e8982f9a6095e347d6bc51d4134ce8a6
Dani Guerrero Camacho <dani.guerrero.camacho@gmail.com>
2025-09-23 19:05:55 +0200

    Fix emails

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -6,29 +6,41 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(req) {
   try {
     const { name, email, message, type } = await req.json();
-
-    if (!name || !email || !message) {
+    console.log("Received data:", { name, email, message, type });
+    if (
+      (type === "simple" && !email) ||
+      (type === "complete" && (!name || !email || !message))
+    ) {
       return NextResponse.json(
         { success: false, error: "Faltan campos" },
         { status: 400 }
       );
     }
 
-    const TYPE_LITERAL =
-      type === "simple"
-        ? "Formulario Simple"
-        : type === "complete"
-        ? "Formulario Completo"
-        : "Formulario";
-
-    await resend.emails.send({
+    if (type === "simple") {
+        await resend.emails.send({
       from: process.env.EMAIL_FROM,
       to: process.env.EMAIL_TO,
-      subject: TYPE_LITERAL,
+      subject: "Formulario Simple",
+      html: `Alguien ha enviado un formulario simple:<br/>
+             <p><strong>Email:</strong> ${email}</p>`,
+    });
+    } else if (type === "complete") {
+        await resend.emails.send({
+      from: process.env.EMAIL_FROM,
+      to: process.env.EMAIL_TO,
+      subject: "Formulario Completo",
       html: `<p><strong>Nombre:</strong> ${name}</p>
              <p><strong>Email:</strong> ${email}</p>
              <p><strong>Mensaje:</strong> ${message}</p>`,
     });
+    } else {
+        return NextResponse.json(
+        { success: false, error: "Tipo de formulario inválido" },
+        { status: 400 }
+      );
+    }
+    
 
     return NextResponse.json({ success: true });
   } catch (error) {
